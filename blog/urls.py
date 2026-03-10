f90df75d23ddea2cc1fbea1fbe612596b8056c33
Cesar-eav <cesar.eav@hotmail.es>
2026-03-10 08:37:56 -0300

    New: Blog App

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.tours_home, name="tours_home" ),
+     path('', views.blog_home, name="blog_home" ),
     # path('tour/<slug:slug>/', views.tour, name="tour")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+
+def blog_home(request):
+
+    posts = Post.objects.all()
+
+    return render(request, 'blog/blog_home.html',{
+        'posts': posts
+    })
