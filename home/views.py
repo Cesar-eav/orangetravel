f9151841617440f44a3f8c0ba69d0f47793c92e0
Cesar-eav <cesar.eav@hotmail.es>
2026-03-10 10:41:59 -0300

    Reseñas al Home

--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from tours.models import Tour
+from blog.models import Post
 
 # Create your views here.
 
 def home(request):
 
     tours = Tour.objects.filter(destacado=True).select_related('precio')
+    posts = Post.objects.order_by('-fecha_creacion')
 
     return render(request, 'home/index.html',{
-        'tours': tours
+        'tours': tours,
+        'posts': posts
     })
