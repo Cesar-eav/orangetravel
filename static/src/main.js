88163453eb1e2dbe80540862bf4d59e352218044
Cesar-eav <cesar.eav@hotmail.es>
2026-04-01 15:59:53 -0300

    alert main

--- static/src/main.js
+++ static/src/main.js
@@ -3,6 +3,8 @@ console.log(
   "background: #f97316; color: white; padding: 5px 10px; border-radius: 5px; font-weight: bold;"
 );
 
+alert("LOCOOOOOOO");
+
 import { createApp } from 'vue';
 import BookingApp from './components/BookingApp.vue';
 
